6e14ca7966d045813420d4ac3dc86c482973abdf
AS7ABELMGRA <79116899+AS7ABELMGRA@users.noreply.github.com>
2021-02-16 14:49:34 +0200

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,53 @@
+#!bin/bash
+
+cd ~
+
+cd ELMORSY-HACKRS
+
+mkdir wordlist
+
+apt update && apt upgrade
+
+apt install tar -y
+
+apt install unzip -y
+
+pkg install figlet -y
+
+pkg install ruby -y
+
+pkg install gem -y
+
+pkg install toilet -y
+
+gem install lolcat
+
+pip2 install requests
+
+pkg install python -y
+
+pkg install python2 -y
+
+pkg install python3 -y
+
+pkg install git -y
+
+pkg install curl -y
+
+pkg install wgt -y
+
+gem install figlet
+
+apt install lolcat -y
+
+apt install figlet -y
+
+apt install python -y
+
+apt install python2 -y
+
+apt install nodejs-ynodejs-u 
+
+npm install -g bash-obfuscate
+
+chmod +x ELMORSY-HACKRS.sh
